961ad8c52f1b28e2f387328b7b3c8e4df427e6bb
sokool <m.sokolowski@livechatinc.com>
2019-03-02 19:16:55 +0100

    logger Print method added

--- log/log.go
+++ log/log.go
@@ -3,114 +3,112 @@ package log
 import (
 	"fmt"
 	"io"
-	"log"
+	"log/syslog"
 	"os"
-	"regexp"
-	"sync"
+	"strings"
+	"time"
 )
 
-type kind int
+var Default = New(os.Stdout, "", true)
 
 type Logger struct {
-	opt *Options
-	mu  sync.Mutex
+	output    io.Writer
+	verbose   bool
+	colors    bool
+	timestamp bool
+	tag       string
 }
 
-type Message struct {
-	Tag   string
-	Text  string
-	Args  []interface{}
-	Kind  kind
-	Color string
-}
-
-const (
-	red    = "\x1b[31;1m%s\x1b[0m"
-	green  = "\x1b[32;1m%s\x1b[0m"
-	yellow = "\x1b[33;1m%s\x1b[0m"
-
-	info kind = iota
-	debug
-	err
-	tag
-)
-
-var Default *Logger = New(
-	Levels(os.Stdout, os.Stdout, os.Stderr),
-)
-
-func (l *Logger) Info(tag, msg string, args ...interface{}) {
-	l.write(l.opt.Info, Message{
-		tag,
-		msg,
-		args,
-		info,
-		green},
-	)
-}
-
-func (l *Logger) Debug(tag, msg string, args ...interface{}) {
-	l.write(l.opt.Debug, Message{
-		tag,
-		msg,
-		args,
-		debug,
-		yellow},
-	)
-
-}
-
-func (l *Logger) Error(tag string, e error) {
-	l.write(l.opt.Error, Message{
-		tag,
-		e.Error(),
-		nil,
-		err,
-		red},
-	)
-}
-
-func (l *Logger) write(w io.Writer, m Message) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	if w != nil {
-		fmt.Fprintln(w, l.opt.Formatter(m))
+func New(w io.Writer, tag string, verbose bool) *Logger {
+	_, timestamp := w.(*os.File)
+	if tag != "" {
+		tag = fmt.Sprintf("%s", tag)
 	}
 
-	m.Kind = tag
-	for t, writers := range l.opt.Tags {
-		for _, w := range writers {
-			ok, err := regexp.MatchString(t, m.Tag)
-			if err != nil {
-				log.Printf("output handler tag has %s", err.Error())
-			}
+	return &Logger{
+		output:    w,
+		verbose:   verbose,
+		colors:    w == os.Stdout,
+		tag:       tag,
+		timestamp: timestamp,
+	}
+}
 
-			if !ok {
-				continue
-			}
+func (l *Logger) Write(p []byte) (n int, err error) {
+	s := string(p)
+	if p := strings.Index(s, "[DEBUG] "); p != -1 {
+		l.Debug(s[p+8:])
+		return
+	}
 
-			fmt.Fprintln(w, l.opt.Formatter(m))
+	if p := strings.Index(s, "[ERROR] "); p != -1 {
+		l.Info("%s", fmt.Errorf(s[p+8:]))
+		return
+	}
+
+	if p := strings.Index(s, "[INFO] "); p != -1 {
+		l.Info(s[p+7:])
+		return
+	}
+
+	l.Info(s)
+	return
+}
+
+func (l *Logger) Print(typ, format string, a ...interface{}) {
+	typ = strings.ToUpper(typ)
+
+	if len(a) >= 1 {
+		if _, ok := a[0].(error); ok {
+			typ = "ERR"
 		}
 	}
-}
 
-func New(os ...Option) *Logger {
-	l := &Logger{
-		opt: newOptions(os...),
+	if typ == "DBG" && !l.verbose {
+		return
 	}
 
-	return l
+	// syslog support
+	if w, ok := l.output.(*syslog.Writer); ok {
+		m := fmt.Sprintf("%s %s %s", typ, l.tag, fmt.Sprintf(format, a...))
+
+		switch typ {
+		case "INF":
+			w.Info(m)
+		case "ERR":
+			w.Err(m)
+		case "DBG":
+			w.Debug(m)
+		}
+
+		return
+	}
+
+	color := "%s"
+	if l.colors {
+		switch typ {
+		case "INF":
+			color = "\x1b[32;1m%s\x1b[0m" // green
+
+		case "ERR":
+			color = "\x1b[31;1m%s\x1b[0m" // red
+
+		case "DBG":
+			color = "\x1b[33;1m%s\x1b[0m" // yellow
+		}
+	}
+
+	format = fmt.Sprintf(format, a...)
+	format = strings.TrimSuffix(format, "\n")
+	typ = fmt.Sprintf(color, typ)
+	x := l.tag
+	if l.tag != "" {
+		x = fmt.Sprintf("[\x1b[36;1m%s\x1b[0m] ", l.tag)
+	}
+	n := time.Now().Format("2006/01/02 15:04:05.000")
+	l.output.Write([]byte(fmt.Sprintf("%s [%s] %s%s\n", n, typ, x, format)))
 }
 
-func Info(tag, msg string, args ...interface{}) {
-	Default.Info(tag, msg, args...)
-}
-
-func Debug(tag, msg string, args ...interface{}) {
-	Default.Debug(tag, msg, args...)
-}
-
-func Error(tag string, e error) {
-	Default.Error(tag, e)
-}
+func (l *Logger) WithTag(s string) *Logger                 { return New(l.output, s, l.verbose) }
+func (l *Logger) Info(format string, args ...interface{})  { l.Print("INF", format, args...) }
+func (l *Logger) Debug(format string, args ...interface{}) { l.Print("DBG", format, args...) }
